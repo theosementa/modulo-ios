03cbf35c6b68e4412258f5816c18320ff70a7083
Theo Sementa <theosementa@gmail.com>
2026-03-22 11:30:59 +0100

    feat: setup commit

---
+++ Modulo/App.swift
@@ -0,0 +1,18 @@
+//
+//  ModuloApp.swift
+//  Modulo
+//
+//  Created by Theo Sementa on 22/03/2026.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ModuloApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootScreen()
+        }
+    }
+}
---
+++ Modulo/RootScreen.swift
@@ -0,0 +1,18 @@
+//
+//  RootScreen.swift
+//  Modulo
+//
+//  Created by Theo Sementa on 22/03/2026.
+//
+
+import SwiftUI
+
+struct RootScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RootScreen()
+}
