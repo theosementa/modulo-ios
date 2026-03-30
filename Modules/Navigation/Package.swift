68b47d5f948208bf0e364fda4f934539f40dff08
Theo Sementa <theosementa@gmail.com>
2026-03-30 16:56:22 +0200

    chore: update packages version

--- Modules/Navigation/Package.swift
+++ Modules/Navigation/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/theosementa/NavigationKit", branch: "3.0.2")
+        .package(url: "https://github.com/theosementa/NavigationKit", branch: "3.0.3")
     ],
     targets: [
         .target(
