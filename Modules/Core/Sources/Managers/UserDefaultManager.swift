465256a3d605202660a7870c1e79bdd1276c5e7e
Theo Sementa <theosementa@gmail.com>
2026-04-04 08:48:46 +0200

    feat: onboarding UI

--- Modules/Core/Sources/Managers/UserDefaultManager.swift
+++ Modules/Core/Sources/Managers/UserDefaultManager.swift
@@ -12,6 +12,10 @@ import Models
 public final class UserDefaultManager: @unchecked Sendable {
     public static let shared = UserDefaultManager()
     
+    @AppStorage("isOnboardingNeedToBePresented")
+    public var isOnboardingNeedToBePresented: Bool = true
+    
+    // MARK: - Preferences
     @AppStorage("isHapticFeebackEnabled")
     public var isHapticFeebackEnabled: Bool = true
     
